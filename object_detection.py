343856d804debe212dab9c01828488f6e2b006cf
Lucas Mongrain <xeaprog@gmail.com>
2019-06-22 22:25:48 -0400

    remove split_model

--- object_detection.py
+++ object_detection.py
@@ -82,91 +82,13 @@ class ObjectDetection:
         if not self.split_model:
             rospy.loginfo('Not spliting model for inference')
             detection_graph = tf.Graph()
-	    with detection_graph.as_default():
+            with detection_graph.as_default():
                 od_graph_def = tf.GraphDef()
                 with tf.gfile.GFile(self.model_path, 'rb') as fid:
                     serialized_graph = fid.read()
                     od_graph_def.ParseFromString(serialized_graph)
                     tf.import_graph_def(od_graph_def, name='')
             return detection_graph, None, None
-        else:
-            rospy.loginfo('Spliti for optimized inference')
-            # load a frozen Model and split it into GPU and CPU graphs
-            # Hardcoded for ssd_mobilenet
-            input_graph = tf.Graph()
-            config = tf.ConfigProto()
-            config.gpu_options.allow_growth = True
-            with tf.Session(graph=input_graph,config=config):
-                if self.ssd_shape == 600:
-                    shape = 7326
-                else:
-                    shape = 1917
-                score = tf.placeholder(tf.float32, shape=(None, shape, self.num_classes),
-                                       name="Postprocessor/convert_scores")
-                expand = tf.placeholder(tf.float32, shape=(None, shape, 1, 4),
-                                        name="Postprocessor/ExpandDims_1")
-                for node in input_graph.as_graph_def().node:
-                    if node.name == "Postprocessor/convert_scores":
-                        score_def = node
-                    if node.name == "Postprocessor/ExpandDims_1":
-                        expand_def = node
-            detection_graph = tf.Graph()
-            with detection_graph.as_default():
-                od_graph_def = tf.GraphDef()
-                with tf.gfile.GFile(self.model_path, 'rb') as fid:
-                    serialized_graph = fid.read()
-                    od_graph_def.ParseFromString(serialized_graph)
-                    dest_nodes = ['Postprocessor/convert_scores', 'Postprocessor/ExpandDims_1']
-
-                    edges = {}
-                    name_to_node_map = {}
-                    node_seq = {}
-                    seq = 0
-                    for node in od_graph_def.node:
-                        n = self._node_name(node.name)
-                        name_to_node_map[n] = node
-                        edges[n] = [self._node_name(x) for x in node.input]
-                        node_seq[n] = seq
-                        seq += 1
-                    for d in dest_nodes:
-                        assert d in name_to_node_map, "%s is not in graph" % d
-
-                    nodes_to_keep = set()
-                    next_to_visit = dest_nodes[:]
-
-                    while next_to_visit:
-                        n = next_to_visit[0]
-                        del next_to_visit[0]
-                        if n in nodes_to_keep:
-                            continue
-                        nodes_to_keep.add(n)
-                        next_to_visit += edges[n]
-
-                    nodes_to_keep_list = sorted(list(nodes_to_keep), key=lambda n: node_seq[n])
-                    nodes_to_remove = set()
-
-                    for n in node_seq:
-                        if n in nodes_to_keep_list:
-                            continue
-                        nodes_to_remove.add(n)
-                    nodes_to_remove_list = sorted(list(nodes_to_remove), key=lambda n: node_seq[n])
-
-                    keep = graph_pb2.GraphDef()
-                    for n in nodes_to_keep_list:
-                        keep.node.extend([copy.deepcopy(name_to_node_map[n])])
-
-                    remove = graph_pb2.GraphDef()
-                    remove.node.extend([score_def])
-                    remove.node.extend([expand_def])
-                    for n in nodes_to_remove_list:
-                        remove.node.extend([copy.deepcopy(name_to_node_map[n])])
-
-                    with tf.device('/gpu:0'):
-                        tf.import_graph_def(keep, name='')
-                    with tf.device('/cpu:0'):
-                        tf.import_graph_def(remove, name='')
-            return detection_graph, score, expand
-
 
     def load_labelmap(self):
         rospy.loginfo('Loading labelmap from label_map.pbtxt')
