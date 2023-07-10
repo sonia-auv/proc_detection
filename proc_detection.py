7fd5d5dea7f4e1d8bbeb6ff3a9b35d39e8b9efab
LogSonia <log.club.sonia@etsmtl.net>
2023-07-10 19:18:59 -0400

    changed code to be able to handle yolo model

--- proc_detection.py
+++ proc_detection.py
@@ -44,15 +44,14 @@ except Exception as err:
 
 
 class ObjectDetection:
-    def __init__(self): 
+    def __init__(self):
         rospy.init_node('proc_detection')
         rospy.loginfo("found gpu: {}".format(tf.test.gpu_device_name()))
 
-        if(tensorrtEnabled):
+        if (tensorrtEnabled):
             rospy.loginfo("Using tensorrt!")
         else:
             rospy.loginfo("cannot import tensorrt, probably because the gpu is not detected!")
-
 
         self.cv_bridge = CvBridge()
         self.fps_interval = 5
@@ -78,39 +77,46 @@ class ObjectDetection:
 
         self.get_config()
 
-        self.network_subscriber = rospy.Subscriber("/proc_detection/change_network", ChangeNetworkMsg, self.handle_change_network)
+        self.network_subscriber = rospy.Subscriber("/proc_detection/change_network", ChangeNetworkMsg,
+                                                   self.handle_change_network)
         self.network_publisher = rospy.Publisher("/proc_detection/status", ChangeNetworkMsg, queue_size=10)
         self.stop_subscriber = rospy.Subscriber("/proc_detection/stop_topic", Empty, self.stop_topic)
         self.bbox_publisher = rospy.Publisher('/proc_detection/bounding_box', DetectionArray, queue_size=10)
         self.detection_mutex.acquire()
         self.detection_graph = self.load_frozen_model(self.initial_model)
         self.detection_mutex.release()
+        self.yolo_classes = [
+            "Bins_Abydos_1", "Bins_Abydos_2", "Bins_Earth_1", "Bins_Earth_2", "Gate_Abydos", "Gate_Earth",
+            "Glyph_Abydos_1",
+            "Glyph_Abydos_2", "Glyph_Earth_1", "Glyph_Earth_2", "Stargate_Closed", "Stargate_Open"
+        ]
         _thread.start_new_thread(self.detection, ())
         rospy.spin()
-        
-    
+
     def image_msg_callback(self, img):
         self.frame = self.cv_bridge.compressed_imgmsg_to_cv2(img, desired_encoding="bgr8")
-    
+
     def load_graph_def(self, filepath):
         graph_def = tf.GraphDef()
         with tf.gfile.GFile(filepath, "rb") as f:
             serialized_graph = f.read()
         graph_def.ParseFromString(serialized_graph)
         return graph_def
-    
+
     def load_frozen_model(self, model_name):
-        if(model_name != self.prev_model and model_name != "@default"):
+        if (model_name != self.prev_model and model_name != "@default"):
             self.prev_model = model_name
             rospy.loginfo("load a new frozen model {}".format(model_name))
-            model_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "external", 'models' , model_name, 'saved_model')
+            model_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "external", 'models', model_name,
+                                     'saved_model')
             detection_function = tf.function()
             if tensorrtEnabled and self.run_with_tensorrt:
-                output_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "external", 'models' , model_name, 'trt')
+                output_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "external", 'models', model_name,
+                                          'trt')
 
                 if not os.path.exists(output_dir):
                     params = trt.DEFAULT_TRT_CONVERSION_PARAMS
-                    params = params._replace(precision_mode = self.trt_precision_mode)
+                    params = params._replace(precision_mode=self.trt_precision_mode)
                     converter = trt.TrtGraphConverterV2(input_saved_model_dir=model_dir, conversion_params=params)
 
                     matrix_type = np.float32
@@ -121,10 +127,11 @@ class ObjectDetection:
                         matrix_type = np.float16
                     elif self.trt_precision_mode == "INT8":
                         matrix_type = np.uint8
-                    
+
                     if self.trt_precision_mode == "INT8":
                         def calibration_input_fn():
-                            inp1 = np.random.normal(size=(1, 1, self.trt_image_width, self.trt_image_height, 3)).astype(matrix_type)
+                            inp1 = np.random.normal(size=(1, 1, self.trt_image_width, self.trt_image_height, 3)).astype(
+                                matrix_type)
                             yield inp1
 
                         converter.convert(calibration_input_fn=calibration_input_fn)
@@ -132,7 +139,8 @@ class ObjectDetection:
                         converter.convert()
 
                     def input_fn():
-                        inp1 = np.random.normal(size=(1, 1, self.trt_image_width, self.trt_image_height, 3)).astype(matrix_type)
+                        inp1 = np.random.normal(size=(1, 1, self.trt_image_width, self.trt_image_height, 3)).astype(
+                            matrix_type)
                         yield inp1
 
                     converter.build(input_fn=input_fn)
@@ -141,20 +149,17 @@ class ObjectDetection:
                 loaded_model = tf.saved_model.load(output_dir, tags=['serve'])
             else:
                 loaded_model = tf.saved_model.load(model_dir)
-            
-            # load label names
-            label_map = label_map_util.load_labelmap(os.path.join(os.path.dirname(os.path.realpath(__file__)), "external", 'models', model_name, 'labelmap.pbtxt'))
-            categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=self.num_classes, use_display_name=True)
-            self.category_index = label_map_util.create_category_index(categories)
+                loaded_model = loaded_model.signatures["serving_default"]
+
             rospy.loginfo("model " + model_name + " is loaded")
-            
+
             return loaded_model
 
         else:
 
             rospy.loginfo("keep the previous model " + model_name)
             return self.detection_graph
-    
+
     def stop_topic(self, data):
         if self.image_subscriber is not None:
             self.image_subscriber.unregister()
@@ -164,42 +169,38 @@ class ObjectDetection:
         if self.image_subscriber is not None:
             self.image_subscriber.unregister()
             self.image_subscriber = None
-        
+
         if data.network_name != self.prev_model:
-            self.prev_model = data.network_name
             self.detection_mutex.acquire()
             self.detection_graph = self.load_frozen_model(data.network_name)
             self.detection_mutex.release()
 
         self.image_subscriber = rospy.Subscriber(data.topic, SensorImage, self.image_msg_callback)
-        self.threshold = data.threshold/100.0
+        self.threshold = data.threshold / 100.0
         self.fps = FPS2(self.fps_interval).start()
-    
-    # function find at: https://github.com/tensorflow/models/blob/75b016b437ab21cbd19dd44451257989fdbb38d6/research/object_detection/colab_tutorials/object_detection_tutorial.ipynb
-    def run_inference_for_single_image(self, model, image):
-        image = np.asarray(image)
-        # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
-        input_tensor = tf.convert_to_tensor(image)
-        # The model expects a batch of images, so add an axis with `tf.newaxis`.
-        input_tensor = input_tensor[tf.newaxis,...]
 
-        # Run inference
-        model_fn = model.signatures['serving_default']
-        output_dict = model_fn(input_tensor)
+    def run_inference_for_single_image(self, model, img):
+        img = np.asarray(img)
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        sq_img = np.zeros((640, 640, 3), dtype=np.uint8)
+        sq_img[120:520, 20:620, :] = img
+        sq_img = np.transpose(sq_img, (2, 0, 1))
+        sq_img = np.expand_dims(sq_img, axis=0)
+        sq_img = sq_img / 255.0
+        tensor = tf.convert_to_tensor(sq_img.astype(np.float32))
+        output = model(images=tensor)["output0"][0].numpy()
+        output = output.transpose()
+        boxes = [row for row in [self.parse_row(row) for row in output] if row[5] > self.threshold]
+        boxes.sort(key=lambda x: x[5], reverse=True)
+        result = []
+        while len(boxes) > 0:
+            result.append(boxes[0])
+            boxes = [box for box in boxes if self.iou(box, boxes[0]) < 0.7]
+        if len(result) != 0:
+            rospy.loginfo(len(result))
+            rospy.loginfo(result)
+        return result
 
-        # All outputs are batches tensors.
-        # Convert to numpy arrays, and take index [0] to remove the batch dimension.
-        # We're only interested in the first num_detections.
-        num_detections = int(output_dict.pop('num_detections'))
-        output_dict = {key:value[0, :num_detections].numpy() 
-                        for key,value in output_dict.items()}
-        output_dict['num_detections'] = num_detections
-
-        # detection_classes should be ints.
-        output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
-            
-        return output_dict
-    
     def detection(self):
         rospy.loginfo("detection thread launched!")
         while not rospy.is_shutdown():
@@ -211,42 +212,39 @@ class ObjectDetection:
             statusmsg.network_name = self.prev_model
             self.network_publisher.publish(statusmsg)
             if self.frame is not None:
-                start = datetime.now()
 
                 image = self.frame
                 self.frame = None
                 image.setflags(write=1)
-                image_expanded = np.expand_dims(image, axis=0)
-                
+
                 self.detection_mutex.acquire()
-                output_dict = self.run_inference_for_single_image(self.detection_graph, image)
+                output_list = self.run_inference_for_single_image(self.detection_graph, image)
                 self.detection_mutex.release()
 
                 list_detection = DetectionArray()
 
-                for i in range(output_dict["detection_boxes"].shape[0]):
-                    if output_dict["detection_scores"] is not None and output_dict["detection_scores"][i] > self.threshold:
-                        detection = Detection()
-                        detection.top = output_dict["detection_boxes"][i][0]
-                        detection.left = output_dict["detection_boxes"][i][1]
-                        detection.bottom = output_dict["detection_boxes"][i][2]
-                        detection.right = output_dict["detection_boxes"][i][3]
+                for output in output_list:
+                    detection = Detection()
+                    detection.top = min(max(output[0] - 120, 0), 400) / 400
+                    detection.left = min(max(output[1] - 20, 0), 600) / 600
+                    detection.bottom = min(max(output[2] - 120, 0), 400) / 400
+                    detection.right = min(max(output[3] - 20, 0), 600) / 600
 
-                        detection.confidence = output_dict["detection_scores"][i]
-                        detection.class_name = str(self.category_index[output_dict["detection_classes"][i]]['name'])
+                    detection.confidence = output[5]
+                    detection.class_name = output[4]
 
-                        list_detection.detected_object.append(detection)
-                        
+                    list_detection.detected_object.append(detection)
+
                 self.bbox_publisher.publish(list_detection)
                 self.fps.update()
             else:
                 time.sleep(0.01)
 
-    
     def get_config(self):
-        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'external', 'config', 'config.yml'), 'r') as ymlfile:
+        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'external', 'config', 'config.yml'),
+                  'r') as ymlfile:
             cfg = yaml.load(ymlfile, Loader=Loader)
-        
+
         self.initial_model = cfg['initial_model']
         self.fps_limit = cfg['fps_limit']
         self.num_classes = cfg['max_num_classes']
@@ -259,6 +257,36 @@ class ObjectDetection:
     def __del__(self):
         self.network_subscriber.unregister()
         self.stop_subscriber.unregister()
+
+    def parse_row(self, row):
+        xc, yc, w, h = row[:4]
+        x1 = (xc - w / 2)
+        y1 = (yc - h / 2)
+        x2 = (xc + w / 2)
+        y2 = (yc + h / 2)
+        prob = row[4:].max(initial=0)
+        class_id = row[4:].argmax()
+        label = self.yolo_classes[class_id]
+        return [x1, y1, x2, y2, label, prob]
+
+    def intersection(self, box1, box2):
+        box1_x1, box1_y1, box1_x2, box1_y2 = box1[:4]
+        box2_x1, box2_y1, box2_x2, box2_y2 = box2[:4]
+        x1 = max(box1_x1, box2_x1)
+        y1 = max(box1_y1, box2_y1)
+        x2 = min(box1_x2, box2_x2)
+        y2 = min(box1_y2, box2_y2)
+        return (x2 - x1) * (y2 - y1)
+
+    def union(self, box1, box2):
+        box1_x1, box1_y1, box1_x2, box1_y2 = box1[:4]
+        box2_x1, box2_y1, box2_x2, box2_y2 = box2[:4]
+        box1_area = (box1_x2 - box1_x1) * (box1_y2 - box1_y1)
+        box2_area = (box2_x2 - box2_x1) * (box2_y2 - box2_y1)
+        return box1_area + box2_area - self.intersection(box1, box2)
+
+    def iou(self, box1, box2):
+        return self.intersection(box1, box2) / self.union(box1, box2)
 
 
 if __name__ == '__main__':
